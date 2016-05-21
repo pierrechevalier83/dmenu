98a512f91e3200751a6e739212050cd9d7fa01cf
Pierre Chevalier <pierrechevalier83@gmail.com>
2016-05-21 21:43:19 +0100

    Solarized, HiDPI

--- config.def.h
+++ config.def.h
@@ -4,15 +4,15 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Tamsyn:size=20"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *normbgcolor = "#222222"; /* -nb option; normal background                 */
-static const char *normfgcolor = "#bbbbbb"; /* -nf option; normal foreground                 */
-static const char *selbgcolor  = "#005577"; /* -sb option; selected background               */
-static const char *selfgcolor  = "#eeeeee"; /* -sf option; selected foreground               */
-static const char *outbgcolor  = "#00ffff";
-static const char *outfgcolor  = "#000000";
+static const char *normbgcolor = "#073642"; /* -nb option; normal background                 */
+static const char *normfgcolor = "#eee8d5"; /* -nf option; normal foreground                 */
+static const char *selbgcolor  = "#268bd2"; /* -sb option; selected background               */
+static const char *selfgcolor  = "#fdf6e3"; /* -sf option; selected foreground               */
+static const char *outbgcolor  = "#073642";
+static const char *outfgcolor  = "#eee8d5";
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
